17868d1e1bdf3d5aa7f59d356729aae1882ee8a2
RayanKamarinchev <rayan2008bg@gmail.com>
2023-03-21 21:54:15 +0200

    softuniada last problems

--- Atoms/Program.cs
+++ Atoms/Program.cs
@@ -1,14 +1,15 @@
 ﻿int n = int.Parse(Console.ReadLine());
-int[] connections = new int[n];
-int[][] weightConnections = new int[n][];
+int[] connections = Enumerable.Repeat(-1, n).ToArray();
+Tuple<int,int>[][] weightConnections = new Tuple<int, int>[n][];
 int[] protConnections = new int[n];
 for (int i = 0; i < n; i++)
 {
     protConnections = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
-    weightConnections[i] = Enumerable.Repeat(-1, n).ToArray();
+    weightConnections[i] = Enumerable.Repeat(new Tuple<int, int>(100, 100), n).ToArray();
     for (int j = protConnections.Length - 1; j >= 0; j--)
     {
-        weightConnections[i][protConnections[j]] += j;
+        var tupl = weightConnections[i][protConnections[j]];
+        weightConnections[i][protConnections[j]] = new Tuple<int, int>(j, tupl.Item2);
     }
 }
 
@@ -16,18 +17,28 @@ int[] elConnections = new int[n];
 for (int i = 0; i < n; i++)
 {
     elConnections = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
-    for (int j = elConnections.Length-1; j >= 0; j--)
+    for (int j = elConnections.Length- 1; j >= 0; j--)
     {
-        weightConnections[elConnections[j]][i]+=j;
+        var tupl = weightConnections[elConnections[j]][i];
+        weightConnections[elConnections[j]][i] = new Tuple<int, int>(tupl.Item1, j);
     }
 }
 
 var listedConnections = weightConnections.Select(c => c.ToList()).ToArray();
-for (int i = 0; i < protConnections.Length; i++)
+connections[0] = 0;
+for (int i = 1; i < protConnections.Length; i++)
 {
-    listedConnections[i].Where((c,i) => connections.Contains(i)).Select(c=>c=0);
-    int indx = listedConnections[i].IndexOf(listedConnections[i].Max());
-    connections[i] = indx;
+    listedConnections[i] = listedConnections[i].Select((c, i) =>
+    {
+        if (connections.Contains(i))
+        {
+            return new Tuple<int, int>(0, 0);
+        }
+
+        return c;
+    }).ToList();
+    var max = listedConnections[i].IndexOf(listedConnections[i].Min());
+    connections[i] = max;
 }
 
 for (int i = 0; i < connections.Length; i++)
@@ -35,4 +46,4 @@ for (int i = 0; i < connections.Length; i++)
     Console.WriteLine($"{i} <-> {connections[i]}");
 }
 
-//40min
+//1h 10min
---
+++ Order2/Program.cs
@@ -0,0 +1,89 @@
+﻿//int n = int.Parse(Console.ReadLine());
+//List<int> ordered = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+
+//List<int> players = new List<int>(ordered);
+//ordered = ordered.OrderBy(p => p).ToList();
+//List<int> one = new List<int>();
+//one.Add(0);
+//List<int> two = new List<int>();
+//two.Add(int.MaxValue);
+//int left = 0;
+
+//for (int i = 0; i < players.Count; i++)
+//{
+//    var oneValid = ordered.Where(n => n > one.Last()).ToList();
+//    int oneIndx = oneValid.IndexOf(players[i]);
+//    var twoValid = ordered.Where(n => n < two.Last()).ToList();
+//    int twoIndx = twoValid.IndexOf(players[i]);
+//    if (twoIndx == -1)
+//    {
+//        twoIndx = int.MinValue;
+//        if (oneIndx == -1)
+//        {
+//            left++;
+//            ordered.Remove(players[i]);
+//            continue;
+//        }
+//    }
+//    if (oneIndx > 1 || twoValid.Count - twoIndx - 1 <= 1)
+//    {
+//        if (twoValid.Count - twoIndx - 1 > 1)
+//        {
+//            left++;
+//        }
+//        else
+//        {
+//            two.Add(players[i]);
+//        }
+//    }
+//    else
+//    {
+//        one.Add(players[i]);
+//    }
+//    ordered.Remove(players[i]);
+//}
+
+//Console.WriteLine(left);
+static int Solve(int n, int index, int whiteIndex, int blackIndex, int[,,] dp, int[] elements)
+{
+    if (index == n)
+    {
+        return 0;
+    }
+
+    if (dp[index, whiteIndex, blackIndex] != 0)
+    {
+        return dp[index, whiteIndex, blackIndex];
+    }
+
+    int secondaryState = 0;
+    int tertiaryState = 0;
+
+    if (whiteIndex == n || elements[index] > elements[whiteIndex])
+    {
+        secondaryState = 1 + Solve(n, index + 1, index, blackIndex, dp, elements);
+    }
+
+    if (blackIndex == n || elements[index] < elements[blackIndex])
+    {
+        tertiaryState = 1 + Solve(n, index + 1, whiteIndex, index, dp, elements);
+    }
+
+    dp[index, whiteIndex, blackIndex] = Math.Max(Solve(n, index + 1, whiteIndex, blackIndex, dp, elements), Math.Max(secondaryState, tertiaryState));
+
+    return dp[index, whiteIndex, blackIndex];
+}
+
+int n = int.Parse(Console.ReadLine());
+
+int[,,] dp = new int[257, 257, 257];
+
+string[] unparsed = Console.ReadLine().Split(' ');
+int[] parsed = new int[n];
+
+for (int i = 0; i < unparsed.Length; i++)
+{
+    parsed[i] = int.Parse(unparsed[i]);
+}
+
+Console.WriteLine(n - Solve(n, 0, n, n, dp, parsed));
