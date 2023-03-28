d6dded46d9b27d28f980d39ee3b580660d8bb5dd
RayanKamarinchev <rayan2008bg@gmail.com>
2023-03-28 09:55:44 +0300

    Softuni 2022

---
+++ Satelites/Program.cs
@@ -0,0 +1,25 @@
+﻿int n = int.Parse(Console.ReadLine());
+Dictionary<string, List<string>> output = new Dictionary<string, List<string>>();
+for (int i = 0; i < n; i++)
+{
+    var els = Console.ReadLine().Split().ToArray();
+    if (!output.ContainsKey(els[0]))
+    {
+        output[els[0]] = new List<string>();
+    }
+    output[els[0]].Add(els[1]);
+
+    if (!output.ContainsKey(els[1]))
+    {
+        output[els[1]] = new List<string>();
+    }
+    output[els[1]].Add(els[0]);
+}
+
+foreach (var key in output.OrderBy(k=>k.Key))
+{
+    foreach (var list in key.Value)
+    {
+        Console.WriteLine($"{key.Key} <-> {list}");
+    }
+}
---
+++ Wires/Program.cs
@@ -0,0 +1,34 @@
+﻿int n = int.Parse(Console.ReadLine());
+int m = int.Parse(Console.ReadLine());
+Dictionary<int, List<int>> connections = new Dictionary<int, List<int>>();
+for (int i = 0; i < m; i++)
+{
+    var inp = Console.ReadLine().Split(" > ").Select(int.Parse).ToArray();
+    if (!connections.ContainsKey(inp[0]))
+    {
+        connections.Add(inp[0], new List<int>());
+    }
+    connections[inp[0]].Add(inp[1]);
+}
+
+List<int> nums = Enumerable.Range(1, n).ToList();
+
+int Solve(List<int> used)
+{
+    if (used.Count()==n)
+    {
+        return 1;
+    }
+    int res = 0;
+    foreach (var i in nums.Where(n => ((connections.ContainsKey(n) && connections[n].All(c => used.Contains(c))) || !connections.ContainsKey(n))&& !used.Contains(n)))
+    {
+        used.Add(i);
+        res+=Solve(used);
+        used.Remove(i);
+    }
+
+    return res;
+}
+
+List<int> used = new List<int>();
+Console.WriteLine(Solve(used));
