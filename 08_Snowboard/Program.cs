e762f74f5694edb909791a7bcd105aa1a546578b
RayanKamarinchev <rayan2008bg@gmail.com>
2023-03-28 14:31:32 +0300

    Sfotuniada 2021 8 and 9

---
+++ 08_Snowboard/Program.cs
@@ -0,0 +1,40 @@
+﻿int n = int.Parse(Console.ReadLine());
+var pathNames = Console.ReadLine().Split().ToArray();
+var costs = Console.ReadLine().Split().Select(int.Parse).ToArray();
+var reward = Console.ReadLine().Split().Select(int.Parse).ToArray();
+List<SnowPath> paths = new List<SnowPath>();
+for (int i = 0; i < pathNames.Length; i++)
+{
+    paths.Add(new SnowPath(pathNames[i], costs[i], reward[i]));
+}
+
+List<SnowPath> used = new List<SnowPath>();
+
+paths = paths.OrderByDescending(p => p.Importance)
+             .ThenByDescending(p=>p.Cost).ToList();
+foreach (var path in paths)
+{
+    if (n>=path.Cost)
+    {
+        n -= path.Cost;
+        used.Add(path);
+    }
+}
+
+Console.WriteLine(string.Join(' ', used.Select(u => u.Name).ToArray()));
+Console.WriteLine($"{used.Select(u => u.Importance * u.Cost).Sum():f0}");
+Console.WriteLine(n);
+
+class SnowPath
+{
+    public string Name { get; set; }
+    public int Cost { get; set; }
+    public decimal Importance { get; set; }
+
+    public SnowPath(string name, int cost, int reward)
+    {
+        Name = name;
+        Cost = cost;
+        Importance = reward * 1.0m / cost;
+    }
+}
---
+++ BinarySort/Program.cs
@@ -0,0 +1,21 @@
+﻿int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+int n = int.Parse(Console.ReadLine());
+n--;
+char[] res = new string('0', a + b).ToCharArray();
+for (int i = 0; i < b; i++)
+{
+    decimal factor = 1m;
+    int p = 0;
+    while (n>=factor)
+    {
+        n -= (int)factor;
+        factor *= (b + p) * 1.0m / (p+1);
+        p++;
+    }
+
+    res[a - p+i] = '1';
+    n++;
+}
+
+Console.WriteLine(res);
